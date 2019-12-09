c4aaf4857d309935b38434a4a2e12ceda33aa73a
fred@macbook <u6127359@anu.edu.au>
2019-12-09 17:36:34 +1100

    add lenet

--- pocket/models/__init__.py
+++ pocket/models/__init__.py
@@ -1,12 +1,6 @@
-"""
-Import package modeuls
+"""Import package modeuls"""
 
-Written by Frederic Zhang
-Australian National University
-
-Last updated in Aug. 2019
-"""
-
+from .lenet import LeNet
 from .roi_projection import *
 from .faster_rcnn import *
 from .multilayer_perceptron import *
