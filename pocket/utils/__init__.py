45474eebda74c5e052bfd4faadeb230e42bc11d6
fred@macbook <u6127359@anu.edu.au>
2019-12-09 17:35:36 +1100

    remove io module

--- pocket/utils/__init__.py
+++ pocket/utils/__init__.py
@@ -1,13 +1,5 @@
-"""
-Import package modules
+"""Import package modules"""
 
-Written by Frederic Zhang
-Australian National University
-
-Last updated in Sept. 2019
-"""
-
-from .io import *
 from .loss import *
 from .meters import *
 from .engines import *
