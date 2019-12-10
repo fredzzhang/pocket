80eb20bb49ce1d0967877056952c596a135681cd
fred@macbook <u6127359@anu.edu.au>
2019-12-10 16:40:43 +1100

    rewrite base class for image datasets

--- pocket/data/base.py
+++ pocket/data/base.py
@@ -1,17 +1,17 @@
 """
 Dataset base classes
 
-Written by Frederic Zhang
-Australian National University
+Fred Zhang <frederic.zhang@anu.edu.au>
 
-Last updated in Aug. 2019
+The Australian National University
+Australian Centre for Robotic Vision
 """
 
 import os
-import cv2
+import json
+import torch
 import pickle
-import warnings
-import numpy as np
+from PIL import Image
 
 class DataDict(dict):
     r"""
@@ -61,190 +61,53 @@ class DataDict(dict):
     def is_empty(self):
         return not bool(len(self))
 
-class ImageDataset:
-    r"""
+class ImageDataset(torch.utils.data.Dataset):
+    """
     Base class for image dataset
 
     Arguments:
-        subset(str): Subset name
-        cfg(CfgNode): Configuration class with the following attributes
-            cfg.DATASET.NAME
-            cfg.DATASET.{subset}.IMAGE_DIR
+        root(str): Root directory where images are downloaded to
+        annFile(str): Path to json annotation file
+        transform(callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version
+        target_transform(callable, optional): A function/transform that takes in the
+            target and transforms it
     """
-    def __init__(self, subset, cfg):
-        """Constructor method"""
-        self._subset = str(subset)
-        self._cfg = cfg
-
-        self._image_paths = self.construct_image_paths(cfg.DATASET[subset].IMAGE_DIR)
-        self._num_images = len(self._image_paths)
-
-        self._num_classes = None
-        self._flip = np.zeros(len(self._image_paths), dtype=np.bool)
+    def __init__(self, root, annoFile, transform=None, target_transform=None):
+        self._root = root
+        self._annoFile = annoFile
+        with open(annoFile, 'r') as f:
+            self._anno = json.load(f)
+        self._transform = transform if transform is not None \
+            else lambda a: a
+        self._target_transform = target_transform if target_transform is not None \
+            else lambda a: a
 
     def __len__(self):
-        """Return the number of images"""
-        return self._num_images
+        raise NotImplementedError
 
     def __getitem__(self, i):
-        """Return an image given the index"""
-        return self.fetch_image(i)
+        raise NotImplementedError
     
     def __repr__(self):
-        """Return the string representation"""
-        reprstr = self.__class__.__name__ + '(' + repr(self._subset) + ', '
-        reprstr += repr(self._cfg)
+        """Return the executable string representation"""
+        reprstr = self.__class__.__name__ + '(root=' + repr(self._root) + ', '
+        reprstr += 'annoFile='
+        reprstr += repr(self._annoFile)
+        reprstr += ', transform='
+        reprstr += repr(self._transform)
+        reprstr += ', target_transform='
+        reprstr += repr(self._target_transform)
         reprstr += ')'
         return reprstr
 
     def __str__(self):
-        """Return database name and subset name"""
-        return '{}-{}'.format(self._cfg.DATASET.NAME, self._subset)
-    
-    @property
-    def num_images(self):
-        """Number of images"""
-        return self._num_images
+        """Return the readable string representation"""
+        reprstr = 'Dataset: ' + self.__class__.__name__ + '\n'
+        reprstr += '\tNumber of images: {}\n'.format(self.__len__())
+        reprstr += '\tRoot path: {}\n'.format(self._root)
+        return reprstr
 
-    @property
-    def num_classes(self):
-        """Number of classes of in the image dataset"""
-        if self._num_classes is None:
-            raise NotImplementedError('Number of classes is not specified')
-        else:
-            return self._num_classes
-    
-    @staticmethod
-    def construct_image_paths(src_dir):
-        """Get the list for the image paths"""
-        im_paths =  [os.path.join(src_dir, f) for f in os.listdir(src_dir) \
-                if f.endswith('.jpg') or f.endswith('.png')]
-        im_paths.sort()
-        return im_paths
-
-    def flip_images(self):
-        """Flip the images for data augmentation"""
-        self._image_paths *= 2
-        self._num_images *= 2
-        self._flip = np.concatenate([self._flip,
-            np.ones(self._num_images, dtype=np.bool)])
-
-    def image_path(self, i):
-        """Return the path of an image given the index"""
-        return self._image_paths[i]
-
-    def fetch_image(self, i):
-        """Return an image [H, W, C] in numpy.ndarray given the index"""
-        im =  cv2.imread(self._image_paths[i])
-        if self._flip[i]:   return im[:, ::-1, :]
-        else:   return im
-
-class DetectionDataset(ImageDataset):
-    r"""
-    Base class for bounding-box detection dataset
-
-    By default, the ground truth and detection bounding boxes are
-    assumed to have been written in .txt files, with the directory
-    paths specified in the argument list. 
-
-    Arguments:
-        subset(str): Subset name
-        cfg(CfgNode): Configuration class with the following attributes
-            cfg.DATASET.NAME
-            cfg.DATASET.SOURCE_FILE_EXTENSION
-            cfg.DATASET.PRINT_INTERVAL
-            cfg.DATASET.{subset}.IMAGE_DIR
-            cfg.DATASET.{subset}.GT_DIR
-            cfg.DATASET.{subset}.GT_CACHE_PATH
-            cfg.DATASET.{subset}.DETECTION_DIR
-            cfg.DATASET.{subset}.DETECTION_CACHE_PATH
-    """
-    def __init__(self, subset, cfg):
-        """Constructor method"""
-        super(DetectionDataset, self).__init__(subset, cfg)
-        self._gtdb = DataDict()
-        self._detdb = DataDict()
-        self._fg_obj = None
-
-    @property
-    def gtdb(self):
-        """The ground truth database"""
-        if self._gtdb.is_empty():
-            self._load_ground_truth()
-        return self._gtdb
-
-    @property
-    def detdb(self):
-        """The detection database"""
-        if self._detdb.is_empty():
-            self._load_detection()
-        return self._detdb
-
-    @staticmethod
-    def construct_src_paths(src_dir, ext='.txt'):
-        """Get the list of all files with a specified extension"""
-        txt_paths = [os.path.join(src_dir, f) for f in os.listdir(src_dir) \
-                if f.endswith(ext)]
-        txt_paths.sort()
-        return txt_paths
-    
-    def _load_from_src_files(self, txt_files):
-        """Load data from .txt files into a list"""
-        db = []
-        for i, f in enumerate(txt_files):
-            with warnings.catch_warnings():
-                warnings.simplefilter('ignore')
-                db.append(np.loadtxt(f, ndmin=2))
-            if i % self._cfg.DATASET.PRINT_INTERVAL == 0:
-                print('Progress: {:>6d}/{:>6d}'.format(i + 1, len(txt_files)))
-        return db
-
-    def _load_ground_truth(self):
-        """Load or construct ground truth database"""
-        if os.path.exists(self._cfg.DATASET[self._subset].GT_CACHE_PATH):
-            print('\nLOADING G.T. DATABASE FROM CACHE...\n')
-            self._gtdb.load(self._cfg.DATASET[self._subset].GT_CACHE_PATH)
-            print('\nDONE\n')
-        else:
-            gt_files = self.construct_src_paths(
-                    self._cfg.DATASET[self._subset].GT_DIR,
-                    self._cfg.DATASET.SOURCE_FILE_EXTENSION)
-            print('\nLOADING G.T. DATA...\n')
-            gt_data = self._load_from_src_files(gt_files)
-            print('\nCONSTRUCTING G.T. DATABASE...\n')
-            self._construct_gtdb(gt_data)
-            self._gtdb.save(self._cfg.DATASET[self._subset].GT_CACHE_PATH)
-            print('\nDONE\n')
-
-    def _load_detection(self):
-        """Load or construct detection database"""
-        if self._gtdb.is_empty():
-            self._load_ground_truth()
-        if os.path.exists(self._cfg.DATASET[self._subset].DETECTION_CACHE_PATH):
-            print('\nLOADING DETECTION DATABASE FROM CACHE...\n')
-            self._detdb.load(self._cfg.DATASET[self._subset].DETECTION_CACHE_PATH)
-            print('\nDONE\n')
-        else:
-            det_files = self.construct_src_paths(
-                    self._cfg.DATASET[self._subset].DETECTION_DIR,
-                    self._cfg.DATASET.SOURCE_FILE_EXTENSION)
-            print('\nLOADING DETECTION DATA...\n')
-            det_data = self._load_from_src_files(det_files)
-            print('\nCONSTRUCTING DETECTION DATABASE\n')
-            self._construct_detdb(det_data)
-            self._detdb.save(self._cfg.DATASET[self._subset].DETECTION_CACHE_PATH)
-            print('\nDONE\n')
-
-    def fg_obj(self, i):
-        """Return the foreground object indices of an image"""
-        if self._fg_obj is None:
-            raise NotImplementedError
-        return self._fg_obj[i]
-
-    def _construct_gtdb(self, gt_data):
-        """Construct ground truth database"""
-        raise NotImplementedError
-
-    def _construct_detdb(self, det_data):
-        """Construct detection database"""
-        raise NotImplementedError
+    def load_image(self, path):
+        """Load an image and apply transform"""
+        return self._transform(Image.open(path))
