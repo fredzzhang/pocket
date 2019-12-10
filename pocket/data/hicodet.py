fc240d62d59f604f15b4abca3578df829d9f1b45
fred@macbook <frederic.zhang@anu.edu.au>
2019-12-10 20:31:11 +1100

    add intra-index

--- pocket/data/hicodet.py
+++ pocket/data/hicodet.py
@@ -23,14 +23,12 @@ class HICODet(ImageDataset):
     """
     def __init__(self, root, annoFile, transform=None, target_transform=None):
         super(HICODet, self).__init__(root, annoFile, transform, target_transform)
-        self._filenames = self._anno['filenames']
-        self._class_corr = self._anno['class']
-        self._empty_idx = self._anno['empty']
-        self._anno = self._anno['annotation']
+        self._idx, self._anno, self._filenames, self._class_corr, self._empty_idx = \
+            self.load_annotation_and_metadata(self._anno)
 
     def __len__(self):
         """Return the number of images"""
-        return len(self._filenames)
+        return len(self._idx)
 
     def __getitem__(self, i):
         """
@@ -40,10 +38,37 @@ class HICODet(ImageDataset):
         Returns:
             tuple[image, target]
         """
-        return self.load_image(os.path.join(self._root, self._filenames[i])), \
-            self._target_transform(self._anno[i])
+        intra_idx = self._idx[i]
+        return self.load_image(os.path.join(self._root, self._filenames[intra_idx])), \
+            self._target_transform(self._anno[intra_idx])
+
+    @property
+    def class_corr(self):
+        """
+        Class correspondence matrix in zero-based index
+        [
+            [hoi_idx, obj_idx, verb_idx],
+            ...
+        ]
+        """
+        return self._class_corr
 
     @staticmethod
     def load_annotation_and_metadata(f):
-        return f['annotation'], f['filenames'], f['class'], f['empty']
+        """
+        Arguments:
+            f(dict): Dictionary loaded from {annoFile}.json
+
+        Returns:
+            list[int]: Indices of images with valid interaction instances
+            list[dict]: Annotations including bounding box pair coordinates and class index
+            list[str]: File names for images
+            list[list]: Class index correspondence
+            list[int]: Indices of images without valid interation instances
+        """
+        idx = list(range(len(f['filenames'])))
+        for empty_idx in f['empty']:
+            idx.remove(empty_idx)
+
+        return idx, f['annotation'], f['filenames'], f['class'], f['empty']
         
