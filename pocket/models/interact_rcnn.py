77477aabc1e65fc42efa49e2d350f2de6d7b1594
fred@macbook <frederic.zhang@anu.edu.au>
2019-12-11 16:41:21 +1100

    name the architecture

---
+++ pocket/models/interact_rcnn.py
@@ -0,0 +1,19 @@
+"""
+Implementation of Interact R-CNN
+
+Fred Zhang <frederic.zhang@anu.edu.au>
+
+The Australian National University
+Australian Centre for Robotic Vision
+"""
+
+import torch
+from torch import nn
+
+class InteractionHead(nn.Module):
+    def __init__(self):
+        pass
+
+class InteractRCNN:
+    def __init__(self):
+        pass
