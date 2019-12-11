0962967f02fdabcc845295af78d2498f467647e7
fred@macbook <frederic.zhang@anu.edu.au>
2019-12-11 16:40:07 +1100

    add average precision meter

--- pocket/utils/meters.py
+++ pocket/utils/meters.py
@@ -1,13 +1,14 @@
 """
 Meters for the purpose of statistics tracking
 
-Written by Frederic Zhang
-Australian National University
+Fred Zhang <frederic.zhang@anu.edu.au>
 
-Last updated in Dec. 2019
+The Australian National University
+Australian Centre for Robotic Vision
 """
 
 import time
+import torch
 
 class Meter:
     """
@@ -64,14 +65,14 @@ class NumericalMeter(Meter):
     def __init__(self, x=[]):
         for item in x:
             assert type(item) in self.VALID_TYPES, \
-                'Given list contains non-numerical element {}'.format(item)
+                "Given list contains non-numerical element {}".format(item)
         super(NumericalMeter, self).__init__(x)
 
     def append(self, x):
         if type(x) in self.VALID_TYPES:
             super(NumericalMeter, self).append(x)
         else:
-            raise TypeError('Given element \'{}\' is not a numeral'.format(x))
+            raise TypeError("Given element \'{}\' is not a numeral".format(x))
 
     def sum(self):
         return sum(self._list)
@@ -97,3 +98,110 @@ class HandyTimer(NumericalMeter):
 
     def __exit__(self, type, value, traceback):
         self.append(time.time() - self._timestamp)
+
+class AveragePrecisionMeter:
+    """
+    Meter to compute average precision
+
+    Arguments:
+        algorithm(str, optional): A choice between '11P' and 'AUC'
+            '11P': 11-point interpolation algorithm prior to voc2010
+            'INT': Interpolation algorithm with all points used in voc2010
+            'AUC': Precisely as the area under precision-recall curve
+        output(tensor[N, K], optinoal): Network outputs with N examples and K classes
+        target(tensor[N, K], optinoal): Binary labels
+    """
+    def __init__(self, algorithm="11P", output=None, target=None):
+        self._output = output if output is not None \
+            else torch.Tensor([])
+        self._target = target if target is not None \
+            else torch.Tensor([])
+
+        self._output_temp = []
+        self._target_temp = []
+
+        if algorithm == "11P":
+            self._eval_alg = self.compute_ap_with_11_point_interpolation
+        elif algorithm == 'INT':
+            self._eval_alg = self.compute_ap_with_interpolation
+        elif algorithm == "AUC":
+            self._eval_alg = self.compute_ap_as_auc
+        else:
+            raise ValueError("Unknown algorithm option {}.".format(algorithm))
+
+    @staticmethod            
+    def compute_ap_as_auc(output, target):
+        """
+        Arguments:
+            output(tensor[N, K])
+            target(tensor[N, K])
+        Returns:
+            tensor[K]
+        """
+        raise NotImplementedError
+    
+    @staticmethod
+    def compute_ap_with_interpolation(output, target):
+        """
+        Arguments:
+            output(tensor[N, K])
+            target(tensor[N, K])
+        Returns:
+            tensor[K]
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    def compute_ap_with_11_point_interpolation(output, target):
+        """
+        Arguments:
+            output(tensor[N, K])
+            target(tensor[N, K])
+        Returns:
+            tensor[K]
+        """
+        raise NotImplementedError
+
+
+    def append(self, output, target):
+        """
+        Add new results to the meter
+
+        Arguments:
+            output(tensor[N, K]): Network output with N examples and K classes
+            target(tensor[N, K]): Binary labels
+        """
+        if isinstance(output, torch.Tensor) and isinstance(target, torch.Tensor):
+            self._output_temp.append(output.detach().cpu())
+            self._target_temp.append(target.detach().cpu())
+        else:
+            raise TypeError("Arguments should both be torch.Tensor")
+
+    def reset(self, keep_tracked=False):
+        """
+        Clear saved statistics
+        """
+        if not keep_tracked:
+            self._output = torch.Tensor([])
+            self._target = torch.Tensor([])
+        self._output_temp = []
+        self._target_temp = []
+
+    def eval(self):
+        """
+        Evaluate the average precision based on collected statistics
+
+        Returns:
+            torch.Tensor[K]: Average precisions for K classes
+        """
+        self._output = torch.cat([
+            self._output,
+            torch.cat(self._output_temp, 0)
+        ], 0)
+        self._target = torch.cat([
+            self._target,
+            torch.cat(self._target_temp, 0)
+        ], 0)
+        self.reset(keep_tracked=True)
+
+        return self._eval_alg(self._output, self._target)
