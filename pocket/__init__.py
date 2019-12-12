afe865b892db4bc4e8418a7d7210cb4b2e717795
fred@macbook <frederic.zhang@anu.edu.au>
2019-12-13 09:53:53 +1100

    update packages

--- pocket/__init__.py
+++ pocket/__init__.py
@@ -1,12 +1,3 @@
-"""
-Import packages
-
-Fred Zhang <frederic.zhang@anu.edu.au>
-
-The Australian National University
-Australian Centre for Robotic Vision
-"""
-
 from . import data
 from . import utils
 from . import models
