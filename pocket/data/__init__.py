af24ad6e7d31c8f3100b727df22498693a0be368
fred@macbook <frederic.zhang@anu.edu.au>
2019-12-13 09:54:13 +1100

    update package modules

--- pocket/data/__init__.py
+++ pocket/data/__init__.py
@@ -1,12 +1,3 @@
-"""
-Import package modules
-
-Fred Zhang <frederic.zhang@anu.edu.au>
-
-The Australian National University
-Australian Centre for Robotic Vision
-"""
-
 from .base import *
 from .hicodet import HICODet
 from .samplers import *
