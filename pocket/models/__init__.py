679a12bc65266472cc53bc0e885e7e1e49f86a4c
fred@macbook <frederic.zhang@anu.edu.au>
2019-12-13 09:54:22 +1100

    update package modules

--- pocket/models/__init__.py
+++ pocket/models/__init__.py
@@ -1,6 +1,4 @@
-"""Import package modeuls"""
-
 from .lenet import LeNet
 from .roi_projection import *
-from .faster_rcnn import *
+from .faster_rcnn import fasterrcnn_resnet, fasterrcnn_resnet_fpn
 from .multilayer_perceptron import *
