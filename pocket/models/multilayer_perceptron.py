b631787518ef24a648b5fad5edda31de15819a7b
fred@macbook <frederic.zhang@anu.edu.au>
2019-12-15 19:48:04 +1100

    add option for norm layer

--- pocket/models/multilayer_perceptron.py
+++ pocket/models/multilayer_perceptron.py
@@ -1,63 +1,14 @@
 """
 Multilayer perceptron based on PyTorch framework
 
-Written by Frederic Zhang
-Australian National University
+Fred Zhang <frederic.zhang@anu.edu.au>
 
-Last updated in Oct. 2019
+The Australian National University
+Australian Centre for Robotic Vision
 """
 
 import torch
 from torch import nn
-
-class MLPwGN(nn.Module):
-    """
-    Multilayer perceptron with group normalization
-    """
-    def __init__(self, dimension, bias, num_groups):
-        super(MLPwGN, self).__init__()
-        self._dimension = dimension
-        self._bias = bias
-        self._num_layer = len(bias)
-        self._num_groups = num_groups
-
-        layers = [nn.Linear(dimension[0], dimension[1], bias[0])]
-        for i in range(1, self._num_layer):
-            layers.append(nn.GroupNorm(num_groups, dimension[i]))
-            layers.append(nn.ReLU())
-            layers.append(nn.Linear(
-                dimension[i],
-                dimension[i + 1],
-                bias[i]
-                ))
-        self.layers = nn.Sequential(*layers)
-
-    def __len__(self):
-        """Return the number of linear layers"""
-        return self._num_layer
-
-    def __repr__(self):
-        reprstr = self.__class__.__name__ + '('
-        reprstr += repr(self._dimension)
-        reprstr += ', '
-        reprstr += repr(self._bias)
-        reprstr += ', '
-        reprstr += repr(self._num_groups)
-        reprstr += ')'
-        return reprstr
-
-    def forward(self, x):
-        # Group norm does not support 0-element inputs
-        if  x.shape[0] == 0:
-            return torch.zeros(0, self._dimension[-1])
-        else:
-            return self.layers(x)
-
-    def freeze_bn(self):
-        pass
-
-    def activate_bn(self):
-        pass
 
 class MultiLayerPerceptron(nn.Module):
     """
@@ -66,39 +17,56 @@ class MultiLayerPerceptron(nn.Module):
     Arguments:
         dimension(list[int]): Dimension of layers in the MLP, starting from input
             layer. The length has to be at least 2
-        bias(list[bool]): Whether to use bias term in a linear layer
-        bathnorm(bool): If True, add batchnorm layers in between linear layers
+        bias(bool or list[bool], optional): If True, use bias terms in linear layers. 
+            If given a bool variable, apply it to all linear layers, otherwise apply 
+            to individual linear layer in order
+        use_norm(bool, optional): If True, use normalization layer before activation (ReLu)
+        norm_layer(callable, optional): Normalization layer to be used
 
     Example:
 
         >>> from pocket.models import MultiLayerPerceptron as MLP
-        >>> net = MLP([5, 20, 10], [True, True])
+        >>> net = MLP([5, 20, 10])
         >>> net.layers
         Sequential(
-          (0): Linear(in_features=5, out_features=20, bias=True)
-          (1): ReLU()
-          (2): BatchNorm1d(20, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-          (3): Linear(in_features=20, out_features=10, bias=True)
+            (0): Linear(in_features=5, out_features=20, bias=True)
+            (1): ReLU()
+            (2): BatchNorm1d(20, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+            (3): Linear(in_features=20, out_features=10, bias=True)
+        )
+        >>> from pocket.models import GroupNormConstructor
+        >>> norm = GroupNormConstructor(32)
+        >>> net = MLP([1024, 1024, 100], bias=[True, False], norm_layer=norm)
+        >>> net.layers
+        Sequential(
+            (0): Linear(in_features=1024, out_features=1024, bias=True)
+            (1): GroupNorm(32, 1024, eps=1e-05, affine=True)
+            (2): ReLU()
+            (3): Linear(in_features=1024, out_features=100, bias=False)
         )
     """
-    def __init__(self, dimension, bias, batchnorm=True):
-        assert len(bias) + 1 == len(dimension),\
-                'Contradictory arguments. Unable to infer the number of linear layers'
+    def __init__(self, dimension, bias=True, use_norm=True, norm_layer=None):
         super(MultiLayerPerceptron, self).__init__()
         self._dimension = dimension
-        self._bias = bias
-        self._num_layer = len(bias)
-        self._batchnorm = batchnorm
+        self._num_layer = len(dimension) - 1
+        self._bias = bias if type(bias) is list \
+            else [bias for _ in range(self._num_layer)]
+        self._use_norm = use_norm
+        self._norm_layer = nn.BatchNorm1d if norm_layer is None \
+            else norm_layer
 
-        layers = [nn.Linear(dimension[0], dimension[1], bias[0])]
+        if not use_norm and norm_layer is not None:
+            print('WARNING: The passed normalization layer is not used')
+
+        layers = [nn.Linear(dimension[0], dimension[1], self._bias[0])]
         for i in range(1, self._num_layer):
-            if batchnorm:
-                layers.append(nn.BatchNorm1d(dimension[i]))
+            if use_norm:
+                layers.append(self._norm_layer(dimension[i]))
             layers.append(nn.ReLU())
             layers.append(nn.Linear(
                 dimension[i],
                 dimension[i + 1],
-                bias[i]
+                self._bias[i]
                 ))
         self.layers = nn.Sequential(*layers)
 
@@ -111,24 +79,12 @@ class MultiLayerPerceptron(nn.Module):
         reprstr += repr(self._dimension)
         reprstr += ', '
         reprstr += repr(self._bias)
-        reprstr += ', '
-        reprstr += repr(self._batchnorm)
+        reprstr += ', use_norm='
+        reprstr += repr(self._use_norm)
+        reprstr += ', norm_layer='
+        reprstr += repr(self._norm_layer)
         reprstr += ')'
         return reprstr
-
-    def freeze_bn(self):
-        for layer in self.layers:
-            if type(layer) == torch.nn.BatchNorm1d:
-                layer.eval()
-                layer.weight.requires_grad = False
-                layer.bias.requires_grad = False
-
-    def activate_bn(self):
-        for layer in self.layers:
-            if type(layer) == torch.nn.BatchNorm1d:
-                layer.train()
-                layer.weight.requires_grad = True
-                layer.bias.requires_grad = True
 
     def forward(self, x):
         # In evaluation mode, BatchNorm module causes error when
