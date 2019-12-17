35a7f6ef7d620487007f38b15f037059f2807144
fred@macbook <frederic.zhang@anu.edu.au>
2019-12-17 12:55:33 +1100

    add a draft of forward pass

--- pocket/models/interact_rcnn.py
+++ pocket/models/interact_rcnn.py
@@ -14,6 +14,40 @@ class InteractionHead(nn.Module):
     def __init__(self):
         pass
 
-class InteractRCNN:
-    def __init__(self):
-        pass
+class InteractRCNN(nn.Module):
+    def __init__(self, backbone, rpn, roi_heads, interaction_heads, transform):
+        super().__init__()
+        self.backbone = backbone
+        self.rpn = rpn
+        self.roi_heads = roi_heads
+        self.interaction_heads = interaction_heads
+        self.transform = transform
+
+    def forward(self, images, targets=None):
+        """
+        Arguments:
+            images (list[Tensor]): images to be processed
+            targets (list[Dict[Tensor]], optional): ground-truth boxes present in the image
+        """
+        if self.training and targets is None:
+            raise ValueError("In training mode, targets should be passed")
+        original_image_sizes = [img.shape[-2:] for img in images]
+        images, targets = self.transform(images, targets)
+        features = self.backbone(images.tensors)
+        proposals, proposal_losses = self.rpn(images, features, targets)
+        detections, detector_losses = self.roi_heads(features, proposals,
+            images.image_sizes, targets)
+        detections, interaction_loss = self.interaction_heads(features, detections,
+            images.image_sizes, targets)    
+        detections = self.transform.postprocess(detections,
+            images.image_sizes, original_image_sizes)
+
+        losses = {}
+        losses.update(detector_losses)
+        losses.update(proposal_losses)
+        losses.update(interaction_loss)
+
+        if self.training:
+            return losses
+
+        return detections
