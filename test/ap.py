c5f374e0908e7f25b78731660debb410a230a25b
fred@macbook <frederic.zhang@anu.edu.au>
2020-01-08 17:25:58 +1100

    test average precision meters

---
+++ test/ap.py
@@ -0,0 +1,64 @@
+"""
+Test average preicision evaluation
+
+Fred Zhang <frederic.zhang@anu.edu.au>
+
+The Australian National University
+Australian Centre for Robotic Vision
+"""
+
+import torch
+from pocket.utils import AveragePrecisionMeter, DetectionAPMeter
+
+def test_1(alg):
+
+    output = torch.rand(100, 4)
+    labels = torch.zeros_like(output)
+
+    labels[:, 0] = 1
+    # NOTE: The AP of a class with a fixed proportion (p) of true positives randomly
+    # selected from the samples has a mathematical expecation of p when computing 
+    # strict AUC. For algorithms with interpolation, the expectation is slightly higher
+    labels[torch.randperm(100)[:30], 2] = 1
+    labels[torch.randperm(100)[:60], 3] = 1
+
+    meter = AveragePrecisionMeter(algorithm=alg, output=output, labels=labels)
+    # Test on-the-fly result collection
+    meter.append(output, labels)
+
+    ap = meter.eval()
+
+    print(ap.tolist())
+
+def test_2(alg):
+    
+    output = [
+        torch.rand(100),
+        torch.rand(200),
+        torch.rand(150),
+        torch.rand(250),
+    ]
+    labels = [
+        torch.zeros(100),
+        torch.ones(200),
+        torch.randint(0, 3, (150,)).clamp(0, 1),
+        torch.randint(0, 2, (250,)).clamp(0, 1),
+    ]
+
+    meter = DetectionAPMeter(4, algorithm=alg, output=output, labels=labels)
+    # Test on-the-fly result collection
+    meter.append(
+        torch.rand(100), 
+        3 * torch.ones(100), 
+        torch.randint(0, 2, (100,)).clamp(0, 1)
+    )
+
+    ap = meter.eval()
+
+    print(ap.tolist())
+
+if __name__ == '__main__':
+    alg = 'AUC'
+
+    test_1(alg)
+    test_2(alg)
