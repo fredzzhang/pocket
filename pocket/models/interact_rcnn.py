94da01a550e311b3e4c02bdb0512ca936d6f88b1
fred@macbook <frederic.zhang@anu.edu.au>
2020-01-10 17:02:50 +1100

    implement transform method for IntearctRCNN

--- pocket/models/interact_rcnn.py
+++ pocket/models/interact_rcnn.py
@@ -11,6 +11,7 @@ import torch
 import torchvision.ops.boxes as box_ops
 from torch import nn
 from torchvision.ops._utils import _cat
+from torchvision.models.detection import transform
 
 class InteractionHead(nn.Module):
     """
@@ -299,7 +300,7 @@ class InteractionHead(nn.Module):
                 verb(Tensor[N])
         Returns:
             loss(dict): During training, return a dict that contains the interaction loss
-            results(list[dict]): During evaluation, return a dict of detected interacitons
+            results(list[dict]): During evaluation, return dicts of detected interacitons
                 "boxes_h": Tensor[M, 4]
                 "boxes_o": Tensor[M, 4]
                 "labels": list(Tensor)
@@ -332,6 +333,45 @@ class InteractionHead(nn.Module):
             class_logits, prior_scores, boxes_h, boxes_o)
 
         return results
+
+class InteractRCNNTransform(transform.GeneralizedRCNNTransform):
+    """
+    Transformations for input image and target (box pairs)
+
+    Arguments(Positional):
+        min_size(int)
+        max_size(int)
+        image_mean(list[float] or tuple[float])
+        image_std(list[float] or tuple[float])
+
+    Refer to torchvision.models.detection for more details
+    """
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def resize(self, image, target):
+        """
+        Override method to resize box pairs
+        """
+        h, w = image.shape[-2:]
+        min_size = float(min(image.shape[-2:]))
+        max_size = float(max(image.shape[-2:]))
+        scale_factor = min(
+            self.min_size[0] / min_size,
+            self.max_size / max_size
+        )
+
+        image = nn.functional.interpolate(image[None], 
+            scale_factor=scale_factor, mode='bilinear', align_corners=False)[0]
+        if target is None:
+            return image, target
+
+        target['boxes_h'] = transform.resize_boxes(target['boxes_h'],
+            (h, w), image.shape[-2:])
+        target['boxes_o'] = transform.resize_boxes(target['boxes_o'],
+            (h, w), image.shape[-2:])
+
+        return image, target
 
 
 class InteractRCNN(nn.Module):
