77b94116dc90bc200a085a41b145efeae3637e0e
fred@woodside <frederic.zhang@anu.edu.au>
2020-01-12 20:15:35 +1100

    fix a syntactic error & add sample output

--- examples/hicodet.py
+++ examples/hicodet.py
@@ -90,7 +90,7 @@ if __name__ == '__main__':
 
     train_loader = DataLoader(
         HICODet(
-            root=(HICO_ROOT, "hico_20160224_det/images/train2015"),
+            root=os.path.join(HICO_ROOT, "hico_20160224_det/images/train2015"),
             annoFile=os.path.join(HICO_ROOT, "instances_train2015.json"),
             transforms=transforms
         ), batch_size=4, shuffle=True, num_workers=4,
@@ -102,10 +102,48 @@ if __name__ == '__main__':
             annoFile=os.path.join(HICO_ROOT, "instances_test2015.json"),
             transforms=transforms
         ), batch_size=4, num_workers=4,
-        collate_fn=custom_collate, drop_last=True
+        collate_fn=custom_collate
     )
 
     engine = MultiLabelClassificationEngine(net, criterion, train_loader,
         val_loader=test_loader, ap_algorithm='11P', print_interval=500)
 
     engine(5)
+
+
+    """
+    Sample output
+
+
+    => Validation (+1754.71s)
+    Epoch: 0 | mAP: 0.0067 | Loss: 0.6940 | Time: 1751.42s
+    
+    ...
+    ...
+    ...
+
+    [Ep.][Iter.]: [5][38000] | Loss: 0.0388 | Time[Data/Iter.]: [0.4588s/203.3094s]
+    [Ep.][Iter.]: [5][38500] | Loss: 0.0383 | Time[Data/Iter.]: [0.0629s/206.3993s]
+    [Ep.][Iter.]: [5][39000] | Loss: 0.0387 | Time[Data/Iter.]: [0.0600s/196.8182s]
+    [Ep.][Iter.]: [5][39500] | Loss: 0.0387 | Time[Data/Iter.]: [0.0607s/208.0784s]
+    [Ep.][Iter.]: [5][40000] | Loss: 0.0383 | Time[Data/Iter.]: [0.0621s/200.6665s]
+    [Ep.][Iter.]: [5][40500] | Loss: 0.0382 | Time[Data/Iter.]: [0.0621s/198.3799s]
+    [Ep.][Iter.]: [5][41000] | Loss: 0.0376 | Time[Data/Iter.]: [0.0643s/206.2081s]
+    [Ep.][Iter.]: [5][41500] | Loss: 0.0378 | Time[Data/Iter.]: [0.0619s/197.4531s]
+    [Ep.][Iter.]: [5][42000] | Loss: 0.0370 | Time[Data/Iter.]: [0.0660s/199.5019s]
+    [Ep.][Iter.]: [5][42500] | Loss: 0.0364 | Time[Data/Iter.]: [0.0620s/202.1312s]
+    [Ep.][Iter.]: [5][43000] | Loss: 0.0370 | Time[Data/Iter.]: [0.0622s/199.2598s]
+    [Ep.][Iter.]: [5][43500] | Loss: 0.0369 | Time[Data/Iter.]: [0.0640s/203.9033s]
+    [Ep.][Iter.]: [5][44000] | Loss: 0.0367 | Time[Data/Iter.]: [0.0625s/200.8627s]
+    [Ep.][Iter.]: [5][44500] | Loss: 0.0358 | Time[Data/Iter.]: [0.0637s/198.3094s]
+    [Ep.][Iter.]: [5][45000] | Loss: 0.0358 | Time[Data/Iter.]: [0.0645s/198.1471s]
+    [Ep.][Iter.]: [5][45500] | Loss: 0.0365 | Time[Data/Iter.]: [0.0633s/206.5252s]
+    [Ep.][Iter.]: [5][46000] | Loss: 0.0362 | Time[Data/Iter.]: [0.0661s/205.7809s]
+    [Ep.][Iter.]: [5][46500] | Loss: 0.0354 | Time[Data/Iter.]: [0.0639s/200.6037s]
+    [Ep.][Iter.]: [5][47000] | Loss: 0.0355 | Time[Data/Iter.]: [0.0596s/201.4554s]
+
+    => Training (+25678.19s)
+    Epoch: 5 | mAP: 0.0114 | Time(eval): 100.55s
+    => Validation (+26641.95s)
+    Epoch: 5 | mAP: 0.0255 | Loss: 0.0284 | Time: 963.73s
+    """
