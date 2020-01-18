42ea8aab5bed1999f99934d656f89039dc618df4
fred@macbook <frederic.zhang@anu.edu.au>
2020-01-18 21:26:09 +1100

    add new module

--- pocket/ops/__init__.py
+++ pocket/ops/__init__.py
@@ -3,3 +3,4 @@ from .masked_roi_align import *
 from .box_pair_poolers import *
 from .transforms import *
 from .group_norm_constructor import GroupNormConstructor
+from .sinkhorn_knopp import SinhornKnoppNorm
