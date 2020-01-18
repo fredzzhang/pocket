a3566e438f1bdd0e09f8d67cbc87d0f12efac020
fred@macbook <frederic.zhang@anu.edu.au>
2020-01-18 21:09:24 +1100

    update module list

--- pocket/models/__init__.py
+++ pocket/models/__init__.py
@@ -1,6 +1,5 @@
 from .lenet import LeNet
 from .roi_projection import *
-from .group_norm import GroupNormConstructor
 from .faster_rcnn import fasterrcnn_resnet, fasterrcnn_resnet_fpn
 from .multilayer_perceptron import *
 from .interact_rcnn import *
