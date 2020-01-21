0a6ea841687e2e687ef9980057b260607408a2db
fred@macbook <frederic.zhang@anu.edu.au>
2020-01-21 16:59:26 +1100

    add softmax layer

--- pocket/models/roi_projection.py
+++ pocket/models/roi_projection.py
@@ -7,6 +7,45 @@ The Australian National University
 Australian Centre for Robotic Vision
 """
 
+"""
+Acknowledgement:
+
+Source code in this module is largely modified from
+https://github.com/pytorch/vision/tree/master/torchvision/models/detection
+
+See below for detailed license
+
+BSD 3-Clause License
+
+Copyright (c) Soumith Chintala 2016, 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
 from torch import nn
 from torch.nn.functional import relu
 from .faster_rcnn import fasterrcnn_resnet_fpn
@@ -72,9 +111,6 @@ class RoIFeatureExtractor(nn.Module):
         Returns:
             Tensor[M, ...]: Features corresponding to different images are stacked in order
         """
-        # Modified from torchvision.models.detection.GeneralizedRCNN
-        # and torchvision.models.detection.RoIHeads
-        # Copyright (c) Facebook, Inc. and its affiliates
         original_image_sizes = [img.shape[-2:] for img in images]
         images, _ = self.detector.transform(images)
         # resize the bounding boxes
@@ -121,6 +157,6 @@ class RoIProjector(RoIFeatureExtractor):
         """
         box_features = super().forward(images, boxes)
         class_logits, _ = self.detector.roi_heads.box_predictor(relu(box_features))
+        pred_scores = nn.functional.softmax(class_logits, -1)
 
-        return box_features, class_logits
-
+        return box_features, pred_scores
