417cc640204849e4cfae0e7b5cf40b4a56ffc2f5
fred@woodside <frederic.zhang@anu.edu.au>
2020-02-05 13:02:44 +1100

    update

--- csrc/setup.py
+++ csrc/setup.py
@@ -1,11 +1,20 @@
+"""
+Setup script for c++ extensions
+
+Fred Zhang <frederic.zhang@anu.edu.au>
+
+The Australian National University
+Australian Centre for Robotic Vision
+"""
+
 from setuptools import setup
 from torch.utils import cpp_extension
 
 setup(
-    name='masks_cpp',
+    name='cpp',
     ext_modules=[
         cpp_extension.CppExtension(
-            name='masks_cpp',
+            name='cpp',
             sources=['masks.cpp']
         )],
     cmdclass={
