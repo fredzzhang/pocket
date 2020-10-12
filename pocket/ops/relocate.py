7bf35894c6519445198bcc2743caa623fbc1783c
fred@macbook <frederic.zhang@anu.edu.au>
2020-10-13 10:46:10 +1100

    Allow keyworded arguments for Tensor.cuda() and Tensor.to()

--- pocket/ops/relocate.py
+++ pocket/ops/relocate.py
@@ -24,46 +24,48 @@ def relocate_to_cpu(x):
     else:
         raise TypeError('Unsupported type of data {}'.format(type(x)))
 
-def relocate_to_cuda(x, device):
+def relocate_to_cuda(x, device, **kwargs):
     """
     Relocate data to CUDA recursively
     
     Arguments:
         x(Tensor, list, tuple or dict)
         device(torch.device or int)
+        kwargs(dict): Refer to torch.Tensor.cuda() for keyworded arguments
     """
     device_id = torch.cuda._utils._get_device_index(device)
     if isinstance(x, torch.Tensor):
-        return x.cuda(device_id)
+        return x.cuda(device_id, **kwargs)
     elif isinstance(x, list):
-        return [relocate_to_cuda(item, device_id) for item in x]
+        return [relocate_to_cuda(item, device_id, **kwargs) for item in x]
     elif isinstance(x, tuple):
-        return (relocate_to_cuda(item, device_id) for item in x)
+        return (relocate_to_cuda(item, device_id, **kwargs) for item in x)
     elif isinstance(x, dict):
         for key in x:
-            x[key] = relocate_to_cuda(x[key], device_id)
+            x[key] = relocate_to_cuda(x[key], device_id, **kwargs)
         return x
     else:
         raise TypeError('Unsupported type of data {}'.format(type(x)))
 
-def relocate_to_device(x, device):
+def relocate_to_device(x, device, **kwargs):
     """
     Relocate data to specified device recursively
 
     Arguments:
         x(Tensor, list, tuple or dict)
         device(torch.device, str or int)
+        kwargs(dict): Refer to torch.Tensor.to() for keyworded arguments
     """
     device = torch.device(device)
     if isinstance(x, torch.Tensor):
-        return x.to(device)
+        return x.to(device, **kwargs)
     elif isinstance(x, list):
-        return [relocate_to_device(item, device) for item in x]
+        return [relocate_to_device(item, device, **kwargs) for item in x]
     elif isinstance(x, tuple):
-        return (relocate_to_device(item, device) for item in x)
+        return (relocate_to_device(item, device, **kwargs) for item in x)
     elif isinstance(x, dict):
         for key in x:
-            x[key] = relocate_to_device(x[key], device)
+            x[key] = relocate_to_device(x[key], device, **kwargs)
         return x
     else:
         raise TypeError('Unsupported type of data {}'.format(type(x)))
