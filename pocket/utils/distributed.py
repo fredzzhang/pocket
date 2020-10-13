74f7c36e3369f961add385e2a55fec92c104e293
fred@macbook <frederic.zhang@anu.edu.au>
2020-10-13 13:33:55 +1100

    Add option for local stats & Fix sync issues

--- pocket/utils/distributed.py
+++ pocket/utils/distributed.py
@@ -14,7 +14,12 @@ from .meters import NumericalMeter
 
 class SyncedNumericalMeter(NumericalMeter):
     """
-    Numerical meter synchronized across subprocesses
+    Numerical meter synchronized across subprocesses. By default, it is assumed that
+    NCCL is used as the communication backend. Communication amongst subprocesses can
+    only be done with CUDA tensors, not CPU tensors. Make sure to intialise default 
+    process group before instantiating the meter by
+
+        torch.distributed.init_process_group(backbone="nccl", ...)
     """
     def __init__(self, maxlen=None):
         if not dist.is_available():
@@ -37,30 +42,58 @@ class SyncedNumericalMeter(NumericalMeter):
         else:
             raise TypeError('Unsupported data type {}'.format(x))
 
-    def sum(self):
-        """Compute the sum across all subprocesses"""
-        sum_ = torch.sum(torch.as_tensor(self._deque))
-        dist.barrier()
-        dist.all_reduce(sum_)
-        return sum_.item()
+    def sum(self, local=False):
+        """
+        Arguments:
+            local(bool): If True, return the local stats.
+                Otherwise, aggregate over all subprocesses
+        """
+        if local:
+            return torch.sum(torch.as_tensor(self._deque)).item()
+        else:
+            sum_ = torch.sum(torch.as_tensor(self._deque, device="cuda"))
+            dist.barrier()
+            dist.all_reduce(sum_)
+            return sum_.item()
 
-    def mean(self):
-        """Compute the mean across all subprocesses"""
-        mean_ = torch.mean(torch.as_tensor(self._deque))
-        dist.barrier()
-        dist.all_reduce(mean_)
-        return mean_ / dist.get_world_size()
+    def mean(self, local=False):
+        """
+        Arguments:
+            local(bool): If True, return the local stats.
+                Otherwise, aggregate over all subprocesses
+        """
+        if local:
+            return torch.mean(torch.as_tensor(self._deque)).item()
+        else:
+            mean_ = torch.mean(torch.as_tensor(self._deque, device="cuda"))
+            dist.barrier()
+            dist.all_reduce(mean_)
+            return mean_.item() / dist.get_world_size()
 
-    def max(self):
-        """Compute the max across all subprocesses"""
-        max_ = torch.max(torch.as_tensor(self._deque))
-        dist.barrier()
-        dist.all_reduce(max_, op=dist.ReduceOp.MAX)
-        return max_
+    def max(self, local=False):
+        """
+        Arguments:
+            local(bool): If True, return the local stats.
+                Otherwise, aggregate over all subprocesses
+        """
+        if local:
+            return torch.max(torch.as_tensor(self._deque)).item()
+        else:
+            max_ = torch.max(torch.as_tensor(self._deque, device="cuda"))
+            dist.barrier()
+            dist.all_reduce(max_, op=dist.ReduceOp.MAX)
+            return max_.item()
 
-    def min(self):
-        """Compute the min across all subprocesses"""
-        min_ = torch.min(torch.as_tensor(self._deque))
-        dist.barrier()
-        dist.all_reduce(min_, op=dist.ReduceOp.MIN)
-        return min_
+    def min(self, local=False):
+        """
+        Arguments:
+            local(bool): If True, return the local stats.
+                Otherwise, aggregate over all subprocesses
+        """
+        if local:
+            return torch.min(torch.as_tensor(self._deque)).item()
+        else:
+            min_ = torch.min(torch.as_tensor(self._deque, device="cuda"))
+            dist.barrier()
+            dist.all_reduce(min_, op=dist.ReduceOp.MIN)
+            return min_.item()
