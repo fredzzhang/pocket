cb2090dcad757fd236565c62d3ef457d4191abfd
fred@macbook <frederic.zhang@anu.edu.au>
2020-10-13 17:43:20 +1100

    Implement all_gather to collect data across subprocesses

--- pocket/utils/distributed.py
+++ pocket/utils/distributed.py
@@ -8,6 +8,7 @@ Australian Centre for Robotic Vision
 """
 
 import torch
+import pickle
 import torch.distributed as dist
 
 from .meters import NumericalMeter
@@ -97,3 +98,54 @@ class SyncedNumericalMeter(NumericalMeter):
             dist.barrier()
             dist.all_reduce(min_, op=dist.ReduceOp.MIN)
             return min_.item()
+
+def all_gather(data):
+    """
+    Run all_gather on arbitrary picklable data (not necessarily tensors)
+    Adapted from https://github.com/pytorch/vision/blob/master/references/detection/utils.py
+
+    Args:
+        data: any picklable object
+    Returns:
+        list[data]: list of data gathered from each rank
+    """
+    world_size = dist.get_world_size()
+    if world_size == 1:
+        return [data]
+
+    not_tensor = type(data) is not torch.Tensor
+
+    if not_tensor:
+        # serialized to a Tensor
+        buffer = pickle.dumps(data)
+        storage = torch.ByteStorage.from_buffer(buffer)
+        tensor = torch.ByteTensor(storage).to("cuda")
+    else:
+        tensor = data.to("cuda")
+
+    # obtain Tensor size of each rank
+    local_size = torch.tensor([tensor.numel()], device="cuda")
+    size_list = [torch.tensor([0], device="cuda") for _ in range(world_size)]
+    dist.all_gather(size_list, local_size)
+    size_list = [int(size.item()) for size in size_list]
+    max_size = max(size_list)
+
+    # receiving Tensor from all ranks
+    # we pad the tensor because torch all_gather does not support
+    # gathering tensors of different shapes
+    tensor_list = []
+    for _ in size_list:
+        tensor_list.append(torch.empty((max_size,), dtype=torch.uint8, device="cuda"))
+    if local_size != max_size:
+        padding = torch.empty(size=(max_size - local_size,), dtype=torch.uint8, device="cuda")
+        tensor = torch.cat((tensor, padding), dim=0)
+    dist.all_gather(tensor_list, tensor)
+
+    if not_tensor:
+        data_list = []
+        for size, tensor in zip(size_list, tensor_list):
+            buffer = tensor.cpu().numpy().tobytes()[:size]
+            data_list.append(pickle.loads(buffer))
+        return data_list
+    else:
+        return [tensor.cpu()[:size] for size, tensor in zip(size_list, tensor_list)]
