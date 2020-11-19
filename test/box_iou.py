1db5cd68a011f510c484f4296de8eb50445ca720
fred@macbook <frederic.zhang@anu.edu.au>
2020-11-19 15:10:43 +1100

    Add test for box iou

---
+++ test/box_iou.py
@@ -0,0 +1,30 @@
+"""
+Test box IoU computation
+
+Fred Zhang <frederic.zhang@anu.edu.au>
+
+The Australian National University
+Australian Center for Robotic Vision
+"""
+
+import torch
+import unittest
+
+from pocket.ops import box_iou
+
+class TestBoxIoU(unittest.TestCase):
+
+    def compatibility(self):
+        boxes_1 = torch.rand(100, 4) * 256
+        boxes_1[:, 2:] += boxes_1[:, :2]
+        boxes_2 = torch.rand(100, 4) * 256
+        boxes_2[:, 2:] += boxes_2[:, :2]
+        iou_1 = box_iou(boxes_1, boxes_2, encoding='coord')
+
+        boxes_1 -= 1; boxes_2 -= 1
+        iou_2 = box_iou(boxes_1, boxes_2, encoding='pixel')
+
+        self.assertTrue(torch.all(iou_1 == iou_2))
+
+if __name__ == '__main__':
+    unittest.main()
