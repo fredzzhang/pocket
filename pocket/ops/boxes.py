52b1bb497f89d3e61b3f1ccb3fac6ca0a55174fb
Frederic Zhang <frederic.zhang@anu.edu.au>
2021-10-28 14:38:15 +1100

    Add NMS for boxes and box pairs (#57)
    * Add test for NMS
    
    Signed-off-by: fred@pegasus <frederic.zhang@anu.edu.au>
    
    * Add NMS for boxes and box pairs
    
    Signed-off-by: fred@pegasus <frederic.zhang@anu.edu.au>
    
    * Fix incorrect variable name
    
    Signed-off-by: fred@pegasus <frederic.zhang@anu.edu.au>

--- pocket/ops/boxes.py
+++ pocket/ops/boxes.py
@@ -8,23 +8,130 @@ Australian Centre for Robotic Vision
 """
 
 import torch
-from torchvision.ops import boxes
+import torchvision.ops.boxes as box_ops
 
 Tensor = torch.Tensor
+
+def nms(boxes: Tensor, scores: Tensor, iou_threshold: float) -> Tensor:
+    """
+    Perform non-maximum suppression (NMS) on boxes
+
+    Parameters:
+    -----------
+    boxes: torch.Tensor
+        Bounding box coordinates formatted as [[x1, y1, x2, y2],...]
+    scores: torch.Tensor
+        Scores for bounding boxes
+    iou_threshold: float
+        IoU threshold for identify overlapping boxes
+
+    Returns:
+    --------
+    keep: torch.Tensor
+        Indices of kept boxes
+    """
+    criteria = box_ops.box_iou(boxes, boxes) >= iou_threshold
+    active = scores.argsort(descending=True)
+    keep = []
+    while len(active):
+        i = active[0]
+        keep.append(i.item())
+        rm = torch.nonzero(criteria[i]).squeeze(1)
+        active = [k for k in active if k not in rm]
+    keep = torch.as_tensor(keep, device=boxes.device)
+    return keep
+
+def pnms(boxes_1: Tensor, boxes_2: Tensor, scores: Tensor, iou_threshold: float) -> Tensor:
+    """
+    Perform non-maximum suppression (NMS) on box pairs
+
+    Parameters:
+    -----------
+    boxes_1: torch.Tensor
+        Bounding box coordinates formatted as [[x1, y1, x2, y2],...]
+    boxes_2: torch.Tensor
+        Bounding box coordinates formatted as [[x1, y1, x2, y2],...]
+    scores: torch.Tensor
+        Scores for bounding boxes
+    iou_threshold: float
+        IoU threshold for identify overlapping boxes
+
+    Returns:
+    --------
+    keep: torch.Tensor
+        Indices of kept box pairs
+    """
+    criteria = torch.min(
+        box_ops.box_iou(boxes_1, boxes_1),
+        box_ops.box_iou(boxes_2, boxes_2)
+    ) >= iou_threshold
+    active = scores.argsort(descending=True)
+    keep = []
+    while len(active):
+        i = active[0]
+        keep.append(i.item())
+        rm = torch.nonzero(criteria[i]).squeeze(1)
+        active = [k for k in active if k not in rm]
+    keep = torch.as_tensor(keep, device=boxes_1.device)
+    return keep
+
+def batched_pnms(
+    boxes_1: Tensor, boxes_2: Tensor,
+    scores: Tensor, classes: Tensor,
+    iou_threshold: float
+) -> Tensor:
+    """
+    Perform non-maximum suppression (NMS) on box pairs from the same class.
+
+    Parameters:
+    -----------
+    boxes_1: torch.Tensor
+        Bounding box coordinates formatted as [[x1, y1, x2, y2],...]
+    boxes_2: torch.Tensor
+        Bounding box coordinates formatted as [[x1, y1, x2, y2],...]
+    scores: torch.Tensor
+        Scores for bounding boxes
+    iou_threshold: float
+        IoU threshold for identify overlapping boxes
+
+    Returns:
+    --------
+    keep: torch.Tensor
+        Indices of kept box pairs
+    """
+    if boxes_1.numel() == 0 or boxes_2.numel() == 0:
+        return torch.empty((0,), dtype=torch.int64, device=boxes_1.device)
+    else:
+        max_coordinate = torch.max(boxes_1, boxes_2).max()
+        offsets = classes.to(boxes_1) * (max_coordinate + torch.tensor(1).to(boxes_1))
+        bx1_for_nms = boxes_1 + offsets[:, None]
+        bx2_for_nms = boxes_2 + offsets[:, None]
+        keep = pnms(bx1_for_nms, bx2_for_nms, scores, iou_threshold)
+        return keep
 
 def box_iou(boxes_1: Tensor, boxes_2: Tensor, encoding: str = 'coord') -> Tensor:
     """
     Compute intersection over union between boxes
 
-    Arguments:
-        boxes_1: (N, 4) Bounding boxes formatted as [[x1, y1, x2, y2],...]
-        boxes_2: (M, 4) Bounding boxes formatted as [[x1, y1, x2, y2],...]
-        encoding: A string that indicates what the boxes encode
+    Parameters:
+    -----------
+    boxes_1: torch.Tensor
+        (N, 4) Bounding boxes formatted as [[x1, y1, x2, y2],...]
+    boxes_2: torch.Tensor
+        (M, 4) Bounding boxes formatted as [[x1, y1, x2, y2],...]
+    encoding: str
+        A string that indicates what the boxes encode
             'coord': Coordinates of the two corners
             'pixel': Pixel indices of the two corners
+
+    Returns:
+    --------
+    torch.Tensor
+        Intersection over union of size (N, M)
+
     """
     if encoding == 'coord':
-        return boxes.box_iou(boxes_1, boxes_2)
+        return box_ops.box_iou(boxes_1, boxes_2)
     elif encoding == 'pixel':
         w1 = (boxes_1[:, 2] - boxes_1[:, 0] + 1).clamp(min=0)
         h1 = (boxes_1[:, 3] - boxes_1[:, 1] + 1).clamp(min=0)
