0418696b89a26fa8ddf69a4c3e289148157410b0
fred@gcr <frederic.zhang@anu.edu.au>
2023-03-10 07:32:05 +0000

    add utilities related to colours

---
+++ pocket/advis/__init__.py
@@ -0,0 +1,17 @@
+"""
+The advanced visual package
+
+Fred Zhang <frederic.zhang@anu.edu.au>
+
+The Australian National University
+"""
+
+try:
+    import seaborn
+except ImportError:
+    raise ImportError(
+        "pocket.advis requires the package seaborn. "
+        "Please run pip install seaborn."
+    )
+
+from .colours import *
